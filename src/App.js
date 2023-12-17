7e7960222d1b8d0829961e027ddbe934239588e0
YunusEmreUslu <usluyunusemre00@gmail.com>
2023-12-17 16:42:44 +0300

    holding data properly , destructured

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import React, { useState } from "react";
+import FlashcardList from "./FlashcardList";
 
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
   return (
-    <h1> hola
-    </h1>
+    <FlashcardList flashcards={flashcards} />
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: '2 + 2 = ?',
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+    ]
+  },
+  {
+    id: 2,
+    question: 'Naber?',
+    answer: '5',
+    options: [
+      '2',
+      '3',
+      '5',
+    ]
+  }
+]
 
 export default App;
